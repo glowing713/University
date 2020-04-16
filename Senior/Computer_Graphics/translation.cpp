bf87d62c92eb49aae3cea9143396de5ec211d507
glowthem <glowing713@naver.com>
2020-04-16 19:53:58 +0900

    2020-04-16 OpenGL translation

---
+++ Senior/Computer_Graphics/translation.cpp
@@ -0,0 +1,29 @@
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/GLUT.h>
+
+
+void MyDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0.0, 0.0, 300, 300);
+    glColor3f(1.0, 0.0, 0.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.5, 0.5, 0.0);
+    glutSolidTeapot(0.3);
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Composite Transformation");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
+    return 0;
+}
